75222dc6159b014ef7019e2e9fc693103ac81372
Fran Martin Rivas <fmartinrivas2@gmail.com>
2021-11-17 19:24:28 +0100

    feat(jupyter-qgis-notebook): edit plugin's icon and do code refactorizing

--- test/test_jupyter_qgis_dialog.py
+++ test/test_jupyter_qgis_dialog.py
@@ -16,7 +16,7 @@ import unittest
 
 from qgis.PyQt.QtGui import QDialogButtonBox, QDialog
 
-from jupyter_qgis_dialog import JupyterQGISDialog
+from jupyter_qgis_notebook_dialog import JupyterQGISDialog
 
 from utilities import get_qgis_app
 QGIS_APP = get_qgis_app()
